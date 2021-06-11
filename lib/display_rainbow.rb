74102cabc2c8d510ffd0fc71aa5c3e28e4ea2973
Diane Phuong Nguyen <dianenguyen@gmail.com>
2021-06-11 02:12:51 +0000

    Automatically backed up by Learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,21 @@
 # Write your #display_rainbow method here
+def display_rainbow(colors)
+  letters = ["R", "O", "Y", "G", "B", "I", "V"]
+  red = "R: red"
+  orange = "O: orange"
+  yellow = "Y: yellow"
+  green = "G: green"
+  blue = "B: blue"
+  indigo = "I: indigo"
+  violet = "V: violet"
+  colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"]
+  puts "The first letter in the alphabet is: #{letters[4]}"
+  puts "The second letter in the alphabet is: #{letters[3]}"
+  puts "The third letter in the alphabet is: #{letters[5]}"
+  puts "The fourth letter in the alphabet is: #{letters[1]}"
+  puts "The fifth letter in the alphabet is: #{letters[0]}"
+  puts "The sixth letter in the alphabet is: #{letters[6]}"
+  puts "The seventh letter in the alphabet is: #{letters[2]}"
+end
+
+display_rainbow
